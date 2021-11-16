2e5d50165f76ac9c57d1afeebd59d8204b4eba97
eg2data <eg2data@gmail.com>
2021-11-16 18:23:58 +0900

    16th November, finished(1.0 - 3.11)

---
+++ src/server.js
@@ -0,0 +1,45 @@
+// 1. create app
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+const app = express();
+
+const logger = morgan("dev");
+
+// 2. configure app
+app.use(logger);
+app.get("/", (req, res, next) => {
+    return res.send("<h1>test</h1>");
+});
+app.get("/login", (req, res, next) => {
+    return res.send({message: "login test"});
+});
+app.get("/privatespace", (req, res) => {
+    return res.send("<h1>Welcome</h1>")
+})
+
+// 3. open app to the outside
+app.listen(PORT, ()=>console.log(`Server is listening on port http://localhost:${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
